6b27966876f825b87caa0a9e37fd3c008497ec01
Christian Bouwense <christian@bigeye.com>
2024-03-12 11:42:52 -0400

    manual timer

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,16 +1,13 @@
 const goToDetailPage = (page) => {
   window.location = `${window.location.href}pieces/${page}`;
-}
+};
 
 const hideLoader = () => {
-  document
-    .getElementById('loading-screen')
-    .style
-    .visibility = "hidden";
-}
+  document.getElementById("loading-screen").style.visibility = "hidden";
+};
 
 const addShowClassToArt = () => {
-  const art = document.getElementsByClassName('art');
+  const art = document.getElementsByClassName("art");
   if (art.length === 0) {
     setTimeout(() => {
       addShowClassToArt();
@@ -18,20 +15,21 @@ const addShowClassToArt = () => {
     return;
   }
   for (let i = 0; i < art.length; i++) {
-    art[i].classList.add('show');
+    art[i].classList.add("show");
   }
-}
+};
 
 const showArt = () => {
   addShowClassToArt();
 
-  document
-    .getElementById('art-gallery')
-    .style
-    .visibility = "visible";
-}
+  document.getElementById("art-gallery").style.visibility = "visible";
+};
 
-window.addEventListener('load', (e) => {
-  hideLoader();
-  showArt();
-});
+const showGallery = () => {
+  setTimeout(() => {
+    // window.addEventListener("load", (e) => {
+    hideLoader();
+    showArt();
+    // });
+  }, 2000);
+};
